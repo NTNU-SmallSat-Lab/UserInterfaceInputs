dcc4e8e268b8a685005ac8812f1285a1b4ca0aa6
scarcelenf <61995432+scarcelenf@users.noreply.github.com>
2020-09-11 20:45:03 +0200

    Update views.py
    cleaning the code

--- UIdjango/UI/views.py
+++ UIdjango/UI/views.py
@@ -5,13 +5,6 @@ from django.template import loader
 from UI.forms import MissionForm
 
 
-
-#def formulario(request):
-
-#Form1=MissionForm.template
-
-#return render(request, "planning.html", {"form":Form1})
-
 def postmission(request):
 
    if request.method=="POST":
@@ -21,28 +14,10 @@ def postmission(request):
             if MissionForm.is_valid():
 
                 return render(request, "missionplanning.html", {"form":miFormulario})
-                            # info=MissionForm.cleaned_data
-
-                        # return render((request, gracias))
+                          
 
             else: render(request, "missionplanning.html", {"form":miFormulario})
 
    else: miFormulario=MissionForm()
 
    return render(request, "missionplanning.html", {"form":miFormulario})
-
-    #doc_externo=open("C:/Users/scarc/OneDrive/Desktop/UIdjango/UI/templates/missionplanning.html")
-
-    #The external document now is saved inside that variable
-
-    #tpt=Template(doc_externo.read())
-
-    #doc_externo.close()
-
-    #doc_externo=loader.get_template('missionplanning.html')
-
-    #ctx=Context()
-
-    #document=doc_externo.render({})
-
-
