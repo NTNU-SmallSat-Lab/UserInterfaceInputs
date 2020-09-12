28f97164102d1cbf47b9d0d6c590b6949d029a8e
scarcelenf <61995432+scarcelenf@users.noreply.github.com>
2020-09-12 12:41:08 +0200

    Update models.py

--- UIdjango/UIapp/models.py
+++ UIdjango/UIapp/models.py
@@ -2,16 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class template(models.Model):
+class template(models.Model):   #Expansion of the administration panel to other topics beyond user administration
 
     template=models.CharField(max_length=40)
 
     content=models.CharField(max_length=10000)
-
-class option(models.Model):
-
-    priority=models.IntegerField()
-
-    multiple_passes=models.BooleanField()
-
-    starttime=models.IntegerField() #UNIX time stamp
